c0fcf8f28d7e714f68ff6cd7c6c6495e73942ba2
mars <imidcen@gmail.com>
2025-09-07 16:55:03 +0100

    checking if this works

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home'; 
 
 import About from './pages/about'; 
